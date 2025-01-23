6561a8c160f82d8716af2b110554e3b8d25388ba
Jack Zisa <33011337+jack-zisa@users.noreply.github.com>
2025-01-23 16:07:34 -0500

    summary

--- src/main/java/dev/creoii/coursebrowser/Main.java
+++ src/main/java/dev/creoii/coursebrowser/Main.java
@@ -1,5 +1,10 @@
 package dev.creoii.coursebrowser;
 
+/**
+ * package frontend/ is for frontend stuff
+ * package backend/ is for backend stuff
+ * package api/ is to connect the two
+ */
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
